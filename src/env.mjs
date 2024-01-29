045ab8405ffb8b2fb5a906bf53f072b975aa3023
Matt <director@acidgambit.com>
2024-01-28 22:45:52 -0500

    update env.mjs

--- src/env.mjs
+++ src/env.mjs
@@ -17,17 +17,6 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
-    NEXTAUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string() : z.string().url(),
-    ),
     STRAVA_CLIENT_ID: z.string(),
     STRAVA_CLIENT_SECRET: z.string(),
   },
